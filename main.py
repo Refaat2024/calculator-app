d11999edf21ccef57f7887b9bcd75a725ee97b45
Refaat2024 <max65283@gmail.com>
2024-01-19 18:18:52 +0200

    calculator commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from tkinter import *
+import ast
+root = Tk()
+i=0
+def get_number(num):
+   global i
+   display.insert(i,num)
+   i+=1
+
+def get_operation(operator):
+    global i
+    length = len(operator)
+    display.insert(i, operator)
+    i+=length
+
+def clear_all():
+    display.delete(0,END)
+
+def calculate():
+    entire_str = display.get()
+    node = ast.parse(entire_str,mode='eval')
+    result = eval(compile(node,'<string>','eval'))
+    clear_all()
+    display.insert(0,result)
+
+display = Entry(root)
+display.grid(row=1,columnspan=6)
+
+numbers = [1,2,3,4,5,6,7,8,9]
+counter =  0
+for x in range(3):
+    for y in range(3):
+        button_text = numbers[counter]
+        button = Button(root,text=button_text,width=2,height=2,command=lambda text=button_text:get_number(text))
+        counter+=1
+        button.grid(row=x+2,column=y)
+
+
+button = Button(root,text="0",width=2,height=2,command=lambda :get_number(0))
+button.grid(row=5,column=1)
+count = 0
+operations = ['+','-','*','/','**','*3.14','%','(',')','**2']
+for x in range(4):
+    for y in range(3):
+        if count < len(operations):
+            button = Button(root, text=operations[count], width=2, height=2,command=lambda text=operations[count]:get_operation(text))
+            count+=1
+            button.grid(row=x + 2, column=y+3)
+
+Button(root,text="AC",width=2,height=2,command=clear_all).grid(row=5,column=0)
+Button(root,text="=",width=2,height=2,command=calculate).grid(row=5,column=2)
+
+root.mainloop()
